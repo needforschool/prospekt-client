a1949919a25bd785dfc72ea0136c67a4a34193b1
TheoDosSantos <theo@callbruno.com>
2023-01-13 16:50:56 +0100

    📱 add responsive on sign up screen

--- src/screens/SignUp/index.tsx
+++ src/screens/SignUp/index.tsx
@@ -8,30 +8,9 @@ const SignUp: React.FC = () => {
     <Container>
       <FormColumn>
         <ImageContainer>
-          <LogoImage src={Logo} alt="Logo" width={200} height={200} />
+          <LogoImage src={Logo} alt="Logo" width={150} />
         </ImageContainer>
         <Form>
-          <CompanyInfo>
-            <FormGroup>
-              <Title>SIRET</Title>
-              <TextInput
-                type="text"
-                id="siret"
-                name="siret"
-                placeholder="123 568 941 00056"
-              ></TextInput>
-            </FormGroup>
-            <FormGroup>
-              <Title>VAT</Title>
-              <TextInput
-                type="text"
-                id="vat"
-                name="vat"
-                placeholder="FR 32 123568941"
-              ></TextInput>
-            </FormGroup>
-          </CompanyInfo>
-
           <FormGroup>
             <Title>Who are you?</Title>
             <RadioContainer>
@@ -50,11 +29,32 @@ const SignUp: React.FC = () => {
                   id="company"
                   name="identity"
                   value="company"
+                  defaultChecked
                 />
                 <Label htmlFor="company">Company</Label>
               </Radio>
             </RadioContainer>
           </FormGroup>
+          <CompanyInfo>
+            <FormGroupVariant>
+              <Title>SIRET</Title>
+              <TextInput
+                type="text"
+                id="siret"
+                name="siret"
+                placeholder="123 568 941 00056"
+              ></TextInput>
+            </FormGroupVariant>
+            <FormGroupVariant>
+              <Title>VAT</Title>
+              <TextInput
+                type="text"
+                id="vat"
+                name="vat"
+                placeholder="FR 32 123568941"
+              ></TextInput>
+            </FormGroupVariant>
+          </CompanyInfo>
           <FormGroup>
             <Title>Full Name</Title>
             <TextInput
@@ -64,24 +64,26 @@ const SignUp: React.FC = () => {
               placeholder="John Doe"
             ></TextInput>
           </FormGroup>
-          <FormGroup>
-            <Title>Email address</Title>
-            <TextInput
-              type="text"
-              id="email"
-              name="email"
-              placeholder="johndoe@prospekt.com"
-            ></TextInput>
-          </FormGroup>
-          <FormGroup>
-            <Title>Phone number</Title>
-            <TextInput
-              type="text"
-              id="phone"
-              name="phone"
-              placeholder="+33 06 01 02 03 04"
-            ></TextInput>
-          </FormGroup>
+          <CompanyInfo>
+            <FormGroupVariant>
+              <Title>Email address</Title>
+              <TextInput
+                type="text"
+                id="email"
+                name="email"
+                placeholder="johndoe@prospekt.com"
+              ></TextInput>
+            </FormGroupVariant>
+            <FormGroupVariant>
+              <Title>Phone number</Title>
+              <TextInput
+                type="text"
+                id="phone"
+                name="phone"
+                placeholder="+33 06 01 02 03 04"
+              ></TextInput>
+            </FormGroupVariant>
+          </CompanyInfo>
           <FormGroup>
             <Title>Password</Title>
             <TextInput
@@ -102,26 +104,35 @@ const SignUp: React.FC = () => {
 const Container = styled.div`
   display: flex;
   flex-direction: row;
-  background: #141522;
-  height: 100vh;
-  padding: 0 15px;
-  color: #f5f5f5;
+  justify-content: space-between;
+  width: 1200px;
+  min-height: 100vh;
+  margin-right: auto;
+  margin-left: auto;
+  color: ${({ theme }) => theme.colors.greyscale3};
+  @media screen and (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    min-height: unset;
+    padding: 0 32px;
+  }
 `;
 
 const FormColumn = styled.div`
   display: flex;
-  flex: 1;
   flex-direction: column;
-  height: 100vh;
-  // background-color: blue;
-  padding-left: 5px;
+  width: 386px;
+  padding: 24px 0;
+  @media screen and (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    width: 100%;
+  }
 `;
 
 const ImageContainer = styled.div`
   width: 100%;
   display: flex;
   height: unset;
-  padding-left: 120px;
 `;
 
 const LogoImage = styled(Image)`
@@ -133,75 +144,115 @@ const Form = styled.form`
   display: flex;
   flex-direction: column;
   width: fit-content;
-  margin-left: 120px;
+  width: 100%;
   margin-top: 80px;
 `;
 
 const CompanyInfo = styled.div`
   display: flex;
-  flex-direction: row;
+  align-items: flex-end;
+  justify-content: space-between;
+  margin-top: 40px;
+  :not(:nth-of-type(2)) {
+    margin-top: 53px;
+  }
 `;
 
 const FormGroup = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
-  margin-top: 50px;
-  // text-align: center;
+  :not(:first-child) {
+    margin-top: 53px;
+    :after {
+      content: "";
+      position: absolute;
+      bottom: -12px;
+      left: 0px;
+      width: 100%;
+      height: 1px;
+      background-color: ${({ theme }) => theme.colors.greyscale3};
+    }
+  }
 `;
 
-const Title = styled.label`
-  padding-left: 20px;
+const FormGroupVariant = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 183px;
+  :after {
+    content: "";
+    position: absolute;
+    bottom: -12px;
+    left: 0px;
+    width: 100%;
+    height: 1px;
+    background-color: ${({ theme }) => theme.colors.greyscale3};
+  }
+  @media screen and (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    width: calc(50% - 20px);
+  }
 `;
+
+const Title = styled.label``;
 
 const RadioContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  padding-top: 12px;
+  @media screen and (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    justify-content: unset;
+    column-gap: 40px;
+  }
 `;
 
 const RadioInput = styled.input``;
 
 const Radio = styled.div`
+  display: flex;
+  align-items: center;
+  width: 183px;
   padding: 23px 20px;
+  border-radius: 4px;
+  border: 1px solid transparent;
+  :has(${RadioInput}:checked) {
+    border-color: ${({ theme }) => theme.colors.primary1};
+  }
 `;
 
-const Label = styled.label`
-  margin-left: 22px;
-`;
+const Label = styled.label``;
 
 const TextInput = styled.input`
   background-color: transparent;
   border: none;
-  border-bottom: 1px solid #f5f5f5;
-  margin-left: 22px;
   margin-top: 20px;
-  color: #d2d2d2;
+  color: ${({ theme }) => theme.colors.greyscale4};
   outline: none;
 `;
 
 const Submit = styled(Button)`
-  width: 386px;
+  width: 100%;
   height: 40px;
-  background-color: #1a72f8;
-  border-radius: 30px;
-  color: #f5f5f5;
-  font-size: 16px;
-  font-weight: 500;
-  text-align: center;
-  line-height: 30px;
   margin-top: 50px;
-  border: none;
-  cursor: pointer;
-  margin-left: 20px;
+  border-radius: 20px;
 `;
 
 const ImageColumn = styled.div`
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  height: 100vh;
-  background-color: pink;
   position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 692px;
+  margin: 24px 0;
+  border-radius: 24px;
+  background-color: ${({ theme }) => theme.colors.greyscale4};
+  object-fit: cover;
+  object-position: 50% 50%;
+  @media screen and (max-width: ${({ theme }) => theme.breakpoint.tablet}) {
+    width: 100%;
+    height: 50vh;
+  }
 `;
 
 export default SignUp;
